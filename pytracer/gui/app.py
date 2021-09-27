1e691f18196ed8015581ded0b426812066006e2d
yohanchatelain <yohan.chatelain@gmail.com>
2021-09-27 12:08:28 -0400

    Fix assets path for visualizer

--- pytracer/gui/app.py
+++ pytracer/gui/app.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 import dash
+import os
+
+__root__ = os.path.dirname(__spec__.origin)
+assets_folder = os.path.join(__root__, 'assets')
 
 external_stylesheets = [
     "https://github.com/plotly/dash-app-stylesheets/blob/master/dash-oil-and-gas.css"]
 app = dash.Dash(__name__,
                 external_stylesheets=[external_stylesheets],
-                meta_tags=[{"name": "viewport", "content": "width=device-width"}])
+                meta_tags=[{"name": "viewport",
+                            "content": "width=device-width"}],
+                assets_folder=assets_folder)
 server = app.server
